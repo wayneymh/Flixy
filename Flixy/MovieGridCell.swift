43b92a1fd55e370d89916b26ff55d834d4b948d3
Minghe Yang <wayney1121@gmail.com>
2020-10-26 14:06:45 -0700

    Added tab bar controller with Superhero tab

---
+++ Flixy/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixy
+//
+//  Created by Minghe Yang on 10/26/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
